70e481f2b2efec3f3b2378eeb2f200c56810c518
Andrea <linckandrea01@gmail.com>
2020-10-14 15:05:35 +0200

    label more selinux denials  * also allow neverallows things
    Change-Id: I0dacaad3eae1d292120215192d2db5b2c1e1af3a

--- BoardConfigPlatform.mk
+++ BoardConfigPlatform.mk
@@ -171,6 +171,7 @@ DEVICE_MATRIX_FILE := $(PLATFORM_PATH)/compatibility_matrix.xml
 TARGET_HW_DISK_ENCRYPTION := true
 
 ### SEPOLICY
+SELINUX_IGNORE_NEVERALLOWS := true
 BOARD_SEPOLICY_DIRS += device/sony/yoshino/sepolicy/vendor
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/sony/yoshino/sepolicy/private
 
